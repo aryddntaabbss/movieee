32843978f21df104b251f7144940c85dd64c88bd
aryaddinata <aryaddinataabbas017@gmail.com>
2023-10-19 16:25:42 +0900

    Create Carousel

--- src/API.jsx
+++ src/API.jsx
@@ -1,16 +1,26 @@
 import axios from "axios";
 
-const apiKey = process.env.REACT_APP_APIKEY;
 const baseUrl = process.env.REACT_APP_BASEURL;
+const apiKey = process.env.REACT_APP_APIKEY;
 
-export const getMovieList = async () =>
-{
-    const movie = await axios.get( `${ baseUrl }/movie/popular?&page=1&api_key=${ apiKey }` )
-    return movie.data.results
-}
+export const getMovieList = async () => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/movie/popular?page=1&api_key=${apiKey}`
+    );
+    return response.data.results;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const searchMovie = async ( q ) =>
-{
-    const search = await axios.get( `${ baseUrl }/search/movie?query=${ q }&api_key = ${ apiKey } ` )
-    return search.data
-}
+export const searchMovie = async (q) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/search/movie?query=${q}&api_key=${apiKey}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,55 +1,99 @@
 import "./App.css";
+import { Navbar, Container, Nav, Form, Carousel } from "react-bootstrap";
 import { getMovieList, searchMovie } from "./API";
 import { useEffect, useState } from "react";
 
-const App = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
+const App = () =>
+{
+  const [ popularMovies, setPopularMovies ] = useState( [] );
 
-  useEffect(() => {
-    getMovieList().then((result) => {
-      setPopularMovies(result);
-    });
-  }, []);
+  useEffect( () =>
+  {
+    getMovieList().then( ( result ) =>
+    {
+      setPopularMovies( result );
+    } );
+  }, [] );
 
-  const PopularMovieList = () => {
-    return popularMovies.map((movie, i) => {
-      return (
-        <div className="Movie-Wrapper" key={i}>
-          <img
-            className="Movie-Image"
-            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
-            alt="img"
-          />
-          <div className="Movie-Title">{movie.title}</div>
-          <div className="Movie-Date">Release : {movie.release_date}</div>
-          <div className="Movie-Rate">{movie.vote_average}</div>
+  const PopularMovieList = () =>
+  {
+    return popularMovies.map( ( movie, i ) => (
+      <div className="Movie-Wrapper" key={ i }>
+        <img
+          className="Movie-Image"
+          src={ `${ process.env.REACT_APP_BASEIMGURL }/${ movie.poster_path }` }
+          alt="img"
+        />
+        <div className="Movie-Title">{ movie.title }</div>
+        <div className="Movie-Date">
+          Release: { movie.release_date } | ⭐{ movie.vote_average }
         </div>
-      );
-    });
+      </div>
+    ) );
   };
 
-  const search = async (q) => {
-    if (q.length > 3) {
-      const query = await searchMovie(q);
-      setPopularMovies(query.results);
+  const search = async ( q ) =>
+  {
+    if ( q.length > 3 )
+    {
+      const query = await searchMovie( q );
+      setPopularMovies( query.results );
     }
   };
 
-  console.log({ popularMovies: popularMovies });
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Movieee.</h1>
-        <input
-          placeholder="Search..."
-          className="Movie-Search"
-          onChange={({ target }) => search(target.value)}
-        />
-        <div className="Movie-Container">
-          <PopularMovieList />
+      <div className="Navigation">
+        <Navbar className="nav-onTop" variant="dark">
+          <Container>
+            <Navbar.Brand href="/" className="Nav-title">
+              Movieee.
+            </Navbar.Brand>
+            <Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  onChange={ ( { target } ) => search( target.value ) }
+                />
+              </Form>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+
+      <div className="Carousel">
+        <Carousel>
+          { popularMovies.map( ( movie, index ) => (
+            <Carousel.Item key={ index }>
+              <img
+                className="d-block w-100"
+                src={ `${ process.env.REACT_APP_BASEIMGURL }/${ movie.poster_path }` }
+                alt={ movie.title }
+              />
+              <Carousel.Caption>
+                <h3>{ movie.title }</h3>
+                <p>{/* Description of the movie */ }</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ) ) }
+        </Carousel>
+      </div>
+
+      <div className="Title-Container">
+        <h2>Popular Movie</h2>
+      </div>
+      <div className="Movie-Container">
+        <PopularMovieList />
+      </div>
+
+      <footer>
+        <div class="footer-content">
+          <p>&copy; 2023 Your Website Name</p>
         </div>
-      </header>
+      </footer>
     </div>
   );
 };
